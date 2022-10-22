fdfc010dd9710d1382c0fcb79080744332e2d4df
Arthur Picard <arthur.picard7@gmail.com>
2022-10-22 16:50:01 +0200

    restructure static with scss

--- src/components/ItemCard.jsx
+++ src/components/ItemCard.jsx
@@ -66,33 +66,41 @@ const ItemCard = () => {
 
               <div className="item-description">
                 <span className='description-header'>Description</span>
+
+                <div className="description-text">
+                  <span>
+                    You might be surprised to know the same plant that grows your morning coffee beans is a popular, low-maintenance houseplant! Although it’s unlikely this plant will produce berries inside, its attractive shiny green foliage will liven up any interior space.
+                    Each plant is unique; size and shape fluctuate by season so all measurements are shown as a range
+                    Small plant measures between 5–10" tall from the soil line to the top of the foliage
+                    Arrives in a nursery grow pot nestled in your planter choice
+                  </span>
+
+                </div>
+
+                <span className='description-header'>Care guide</span>
+                <div className="description-text">
+                  <span>
+                    Coffee Plant
+                    Africa and Asia
+                    Plant Care
+
+                    Thrives in bright indirect light.
+
+                    Water every 1-2 weeks, allowing soil to dry out half way down between waterings. Expect to water more often in brighter light and less often in lower light.
+
+                    The occasional pruning will help the leafy Coffee Plant branch out into a bushier specimen.
+                    Sad Plant Signs
+                    Yellowing leaves, mushy blackened base:
+                    Overwatered, root rot
+                    Wilting and curling leaves, dry potting mix:
+                    Underwatered, thirsty plant
+                    Brown tips and edges of leaves:
+                    Low humidity
+                  </span>
+
+                </div>
+                <span className='escription-header'>Care guide</span>
                 <span className='description-text'>
-                  You might be surprised to know the same plant that grows your morning coffee beans is a popular, low-maintenance houseplant! Although it’s unlikely this plant will produce berries inside, its attractive shiny green foliage will liven up any interior space.
-                  Each plant is unique; size and shape fluctuate by season so all measurements are shown as a range
-                  Small plant measures between 5–10" tall from the soil line to the top of the foliage
-                  Arrives in a nursery grow pot nestled in your planter choice
-                </span>
-                <span className='care-header'>Care guide</span>
-                <span className='care-text'>
-                  Coffee Plant
-                  Africa and Asia
-                  Plant Care
-
-                  Thrives in bright indirect light.
-
-                  Water every 1-2 weeks, allowing soil to dry out half way down between waterings. Expect to water more often in brighter light and less often in lower light.
-
-                  The occasional pruning will help the leafy Coffee Plant branch out into a bushier specimen.
-                  Sad Plant Signs
-                  Yellowing leaves, mushy blackened base:
-                  Overwatered, root rot
-                  Wilting and curling leaves, dry potting mix:
-                  Underwatered, thirsty plant
-                  Brown tips and edges of leaves:
-                  Low humidity
-                </span>
-                <span className='care-header'>Care guide</span>
-                <span className='care-text'>
                   Coffee Plant
                   Africa and Asia
                   Plant Care
