3454bbed59ecacba588459c7dd337747478f6d54
Arthur Picard <arthur.picard7@gmail.com>
2022-12-06 21:01:21 +0100

    Add Routes to App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ItemCard from './components/ItemCard';
+import Navbar from './components/Navbar';
+import Collection from './components/Collection';
+import Layout from './components/Navbar';
+import Home from './pages/Home/Home';
+
 import './App.css';
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path='/collection' element={<Collection />} />
+        <Route path='/:id' element={<ItemCard />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
